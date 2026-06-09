0281be924dec8c179ae35b3e86eeea40ef9fdc5d
magui-sanchez <sanchezma@udesa.edu.ar>
2026-06-09 10:42:56 -0300

    comienzo y creacion de carpetas

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9UmPTzJkU48FkFfIfCTtUf81fkxu5-Qs",
+  authDomain: "fir-pi-rn.firebaseapp.com",
+  projectId: "fir-pi-rn",
+  storageBucket: "fir-pi-rn.firebasestorage.app",
+  messagingSenderId: "687250574444",
+  appId: "1:687250574444:web:7929f3a7d1f3e297dd9c49"
+};
+
+const app = initializeApp(firebaseConfig);
