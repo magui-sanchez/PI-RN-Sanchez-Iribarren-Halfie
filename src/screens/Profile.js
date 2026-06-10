a6787a47d0e6efe674726c23c32388f903888c91
Hannahalfie6 <halfie@udesa.edu.ar>
2026-06-10 14:56:51 -0300

    hice el logout

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
+import { auth } from "../config/firebase";
 
 function Profile(props) {
+
+    const logout = () => {
+        auth.signOut();
+        props.navigation.navigate("Login");
+    }
 
     return (
         <View>
             <Text>Profile</Text>
-
+            <Pressable onPress={() => logout()}>
+                <Text>Desloguearse</Text>
+            </Pressable>
         </View>
     )
 }
