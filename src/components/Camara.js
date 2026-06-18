c05a7611ecc084fda93fb4946f9506ccc75dd695
delfina-iribarren <diribarren@udesa.edu.ar>
2026-06-18 23:42:17 +0200

    corregi el error que no renderizaba el crear post

--- src/components/Camara.js
+++ src/components/Camara.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { View, Text, Pressable, StyleSheet} from "react-native";
 import {CameraView, Camera} from "expo-camera";
 
